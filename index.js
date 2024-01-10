7a0040a12a3163a9c7711bd46ada1e351c58e8ce
Tanvish <101194211+TanvishGG@users.noreply.github.com>
2024-01-10 16:32:54 +0000

    v1.1.0

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const rps = require('./functions/rps');
 const gtn = require('./functions/gtn');
 const rtc = require('./functions/rtc');
 const cf = require('./functions/cf');
+const fts = require('./functions/fts')
 module.exports = {
     RockPaperScissors: rps,
     GuessTheNumber: gtn,
     RepeatTheColor: rtc,
     CoinFlip: cf,
+    FindTheStone: fts,
 }
